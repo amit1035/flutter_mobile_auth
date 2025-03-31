8e3d1651a42a342249326e08dce874ac29d36c7f
Amit Gupta <129865161+amit1035@users.noreply.github.com>
2025-03-31 10:42:28 +0530

    Updated login page, added new folder, and removed old files

--- lib/screen/LoginScreen.dart
+++ lib/screen/LoginScreen.dart
@@ -68,7 +68,7 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.redAccent,
+        backgroundColor: Colors.green,
         leading: IconButton(
           icon: Icon(Icons.arrow_back, color: Colors.white),
           onPressed: () => Navigator.pop(context),
